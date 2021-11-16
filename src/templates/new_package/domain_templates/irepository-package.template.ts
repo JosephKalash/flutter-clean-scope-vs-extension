876f3c44c74d7131b97eeee031a70027d085430e
Joseph Kalash <39829281+JosephKalash@users.noreply.github.com>
2021-11-16 23:45:49 +0200

    Update irepository-package.template.ts

--- src/templates/new_package/domain_templates/irepository-package.template.ts
+++ src/templates/new_package/domain_templates/irepository-package.template.ts
@@ -4,36 +4,15 @@ export function getRepositoryTemplate(
   name: string,
   isEmptyPackage: boolean
 ): string {
-  return isEmptyPackage
-    ? getBaseRepositoryTemplate(name)
-    : getExampleRepositoryTemplate(name);
-}
-
-function getExampleRepositoryTemplate(name: string): string {
-  const pascalCaseName = changeCase.pascalCase(name.toLowerCase());
-  //   const snakeCaseName = changeCase.snakeCase(name.toLowerCase());
-  return `import 'package:errors/errors.dart';
-import 'package:meta/meta.dart';
-import 'package:dartz/dartz.dart';
-
-/// Repository interface for ${name}
-@immutable
-abstract class I${pascalCaseName}Repository {
-  //TODO: Implement Interface
-
-  //*Example:
-  ///Example of abstract function
-  Future<Either<Failure, int>> exampleFunctionAddition(int currentNumber);
-}
-
-    `;
+  return getBaseRepositoryTemplate(name);
 }
 
 function getBaseRepositoryTemplate(name: string): string {
   const pascalCaseName = changeCase.pascalCase(name.toLowerCase());
   //   const snakeCaseName = changeCase.snakeCase(name.toLowerCase());
-  return `/// Repository interface for ${name}
+  return `import 'package:dartz/dartz.dart';
+  
   abstract class I${pascalCaseName}Repository {
-    // TODO: Define interface methods
+    Future<Either<Failure,List<${name}>>> get${name}();
   }`;
 }
