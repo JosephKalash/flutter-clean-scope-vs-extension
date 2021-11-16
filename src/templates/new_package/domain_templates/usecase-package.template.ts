151a5d3fb8c07c4fd1d641a4417c8abdc9a57214
Joseph Kalash <39829281+JosephKalash@users.noreply.github.com>
2021-11-16 23:51:02 +0200

    Update usecase-package.template.ts

--- src/templates/new_package/domain_templates/usecase-package.template.ts
+++ src/templates/new_package/domain_templates/usecase-package.template.ts
@@ -4,58 +4,20 @@ export function getUseCaseTemplate(name: string): string {
     return getDefaultUseCaseTemplate(name);
 }
 
-export function getExampleUseCaseTemplate(name: string): string {
-    return getDefaultExampleUseCaseTemplate(name);
-}
-
 function getDefaultUseCaseTemplate(name: string): string {
   const pascalCaseName = changeCase.pascalCase(name.toLowerCase());
   const snakeCaseName = changeCase.snakeCase(name.toLowerCase());
   return `import 'package:dartz/dartz.dart';
-import 'package:errors/errors.dart';
-import 'package:meta/meta.dart';
 
 import '../repositories/i${snakeCaseName}_repository.dart';
 
 class UseCase {
-  UseCase({@required this.repository}) : assert(repository != null);
+  UseCase(this.repository);
 
   final I${pascalCaseName}Repository repository;
 
-  /// Callable class method
-  Future<Either<Failure, Object>> call() async {
-    /// TODO: Call specific repository method
+  Future<Either<Failure, List<${name}>>> call() async {
+    return repository.get${name}s();
   }
 }`;
-}
-
-
-function getDefaultExampleUseCaseTemplate(name: string): string {
-  const pascalCaseName = changeCase.pascalCase(name.toLowerCase());
-  const snakeCaseName = changeCase.snakeCase(name.toLowerCase());
-  const cammelCase = changeCase.camelCase(name.toLowerCase());
-  return `import 'package:meta/meta.dart';
-import 'package:dartz/dartz.dart';
-import 'package:errors/errors.dart';
-
-import '../repositories/i${snakeCaseName}_repository.dart';
-
-import 'usecase.dart';
-
-///Increment Counter usecase
-class ExampleAddition implements UseCase<int, int> {
-  /// Example Addition constructor
-  ExampleAddition({
-    @required I${pascalCaseName}Repository ${cammelCase}Repo,
-  })  : assert(${cammelCase}Repo != null),
-        _${cammelCase}Repo = ${cammelCase}Repo;
-
-  final I${pascalCaseName}Repository _${cammelCase}Repo;
-
-  @override
-  Future<Either<Failure, int>> call(int currentNumber) async {
-    return await _${cammelCase}Repo.exampleFunctionAddition(currentNumber);
-  }
-}
-`;
 }
