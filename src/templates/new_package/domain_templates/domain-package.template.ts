ad895a0cedd78c70dcfd910323475bb6e63a980a
Joseph Kalash <39829281+JosephKalash@users.noreply.github.com>
2021-11-17 00:06:04 +0200

    Update domain-package.template.ts

--- src/templates/new_package/domain_templates/domain-package.template.ts
+++ src/templates/new_package/domain_templates/domain-package.template.ts
@@ -4,36 +4,19 @@ export function getDomainTemplate(
   name: string,
   isEmptyProject: boolean
 ): string {
-  return isEmptyProject
-    ? getBaseDomainTemplate(name)
-    : getExampleDomainTemplate(name);
+  return getBaseDomainTemplate(name);
 }
 
 function getBaseDomainTemplate(name: string): string {
-  //   const pascalCaseName = changeCase.pascalCase(name.toLowerCase());
+  const pascalCaseName = changeCase.pascalCase(name.toLowerCase());
   const snakeCaseName = changeCase.snakeCase(name.toLowerCase());
   return `// Export Entities
 // TODO: Export entities
-
+export 'entities/${pascalCaseName}_entity.dart';
 // Export Repositories
 export 'repositories/i${snakeCaseName}_repository.dart';
 
 // Export Use Cases
-// TODO: Export use cases`;
-}
-
-function getExampleDomainTemplate(name: string): string {
-  //   const pascalCaseName = changeCase.pascalCase(name.toLowerCase());
-  const snakeCaseName = changeCase.snakeCase(name.toLowerCase());
-  return `// Entities
-//TODO: export entities
-// export 'entities/your_entity.dart';
-
-// Export Repositories
-export 'repositories/i${snakeCaseName}_repository.dart';
-
-// Export Use Cases
-//TODO: export usecases
-export 'usecases/example_addition.dart';
+export 'usecases/get${snakeCaseName}.dart';
 `;
 }
